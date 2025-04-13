3cf83ef47e8a125efc49c6ace098895299e71b17
Lewis Blackwell <lewisblackwell@Lewiss-Air.fios-router.home>
2025-04-13 15:54:39 -0400

    test

--- test2.py
+++ test2.py
@@ -1,2 +1,11 @@
 import streamlit as st
+import os
+
+# Set the environment variable BEFORE importing chromadb
+os.environ["CHROMA_DB_IMPL"] = "chroma_inmemory"
+
 import chromadb
+
+# Now you can use chromadb without issues
+# Example:
+client = chromadb.Client() #client will be in memory.
